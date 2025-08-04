71e8d46a684ae92a183db6cc9c4862e70b24685c
0xunlin <priyantarjonak9@gmail.com>
2025-08-04 16:25:06 +0000

    ninth commit: "setup for deployement"

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Use 'export' for static export
+  distDir: 'out', // Output directory for the static files
+  images: { // Disable image optimization for static export
+    unoptimized: true,
+  },
+  basePath: '', // Base path for the application
+  assetPrefix: './', // Asset prefix for static files
+  trailingSlash: true, // Add trailing slash to URLs
 };
 
 export default nextConfig;
