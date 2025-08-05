a029adc1f1bb49fe57acc6c5c814d756239c317b
0xunlin <priyantarjonak9@gmail.com>
2025-08-05 15:04:45 +0000

    extra commit: "bug fixes"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import type { Metadata } from "next";
 import "./globals.css";
 import { ReactNode } from "react";
--- src/rainbowKitConfig.tsx
+++ src/rainbowKitConfig.tsx
@@ -1,11 +1,14 @@
 "use client"
 
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { anvil, zksync, mainnet } from "wagmi/chains";
+import { getDefaultConfig } from "@rainbow-me/rainbowkit"
+import { anvil, zksync, mainnet } from "wagmi/chains"
+import { type Config } from "wagmi"
 
-export default getDefaultConfig({
-    appName: "TSender",
-    projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-    chains: [anvil, zksync, mainnet],
-    ssr: false, // server side rendering is set to false beacuse at the moment we want to make it a static site
+const config: Config = getDefaultConfig({
+  appName: "TSender",
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+  chains: [anvil, zksync, mainnet],
+  ssr: false,
 })
+
+export default config
